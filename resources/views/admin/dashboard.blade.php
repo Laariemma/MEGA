d072d82f8ae69bda9c61585c6082bd67d99710bf
Petetsu <peter.rantalainen@student.lab.fi>
2025-03-04 12:35:30 +0200

    kirjautumishommeleita

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,16 +1,16 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Dashboard Admin') }}
         </h2>
     </x-slot>
-
+        
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                    <img src= https://lumiere-a.akamaihd.net/v1/images/open-uri20150422-20810-a07syh_9331bd0a.jpeg
+                    {{ __("Olet kirjautunut adminina") }}
+                    <img src=https://img.ilcdn.fi/0GfrkRcaoZYZRMmg74xrP2xf5IE=/full-fit-in/920x0/img-s3.ilcdn.fi/c56b70965a246d258b45c797c171887c52ccb73e7edb701fb7145dd7df7467f2.jpg
                 </div>
             </div>
         </div>
